01f03c6e36ed64dfe4f17cb3d25488776a6f87e3
TUPOBO <1028461931@qq.com>
2018-06-28 07:46:53 +0800

    add components Header BookInfo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import {Route ,Link} from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
+import ShowSearchPage from './components/ShowSearchPage';
 
 class BooksApp extends React.Component {
   state = {
@@ -18,33 +20,12 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
       <Route path="/ShowSearchPage" render={() => (
-        <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/" className="close-search" >Close</Link>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
-      </div>
+        <ShowSearchPage />
       )} />
          <Route exact path="/" render={
            () => (
             <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
+            <Header />
             <div className="list-books-content">
               <div>
                 <div className="bookshelf">
@@ -56,7 +37,7 @@ class BooksApp extends React.Component {
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                             <div className="book-shelf-changer">
-                              <select>
+                              <select value='wantToRead'>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
